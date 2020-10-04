3361ca93ca651a64381b2c4f06a1f88cdb08ab1d
Nikos Angelopoulos <angelopoulosn2@gmail.com>
2020-10-04 14:52:28 +0300

    Change zsh run.sh to install oh my zsh

--- zsh/run.sh
+++ zsh/run.sh
@@ -5,7 +5,9 @@ path=$(cd "$(pwd)/$(dirname $0)" && pwd)
 
 source $path/../lib.sh
 
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 backup ".zshrc"
 
 ln -s $path/zshrc $HOME/.zshrc
+source ~/.zshrc
